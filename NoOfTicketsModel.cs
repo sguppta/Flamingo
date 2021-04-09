107ab81afb16b1beefd0adcd871bdf6a18e41c9d
sguppta <43344963+sguppta@users.noreply.github.com>
2021-04-09 09:54:21 +0530

    Add files via upload

---
+++ NoOfTicketsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Flamingo_Airways.Models
+{
+    public class NoOfTicketsModel
+    {
+        public int Nooftickets { get; set; }
+    }
+}
---
+++ PaymentModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Flamingo_Airways.Models
+{
+    public class PaymentModel
+    {
+
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Mode(Credit Card/Debit Card)")]
+        public string Mode { get; set; }
+
+        private DateTime myVar;
+
+        public DateTime Paymentdate
+        {
+            //get { return myVar; }
+            set { myVar = DateTime.Now; }
+        }
+
+
+        public decimal Amount { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "Enter your registered email")]
+        public string User_Email { get; set; }
+
+        [Required,RegularExpression(@"^([0-9]{16})$")]
+        [DataType(DataType.CreditCard)]
+        [Display(Name = "enter your Card Number")]
+        public long Cardno { get; set; }
+
+        [Required]
+        
+        [Range(3,3)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Enter CVV")]
+        public int cvv { get; set; }
+    }
+}
